9f495f0750c4651ce3d77ab7564eb5e8639565a3
hopalt <hopalt@gmail.com>
2019-05-29 17:07:50 +0900

    test33

--- test.java
+++ test.java
@@ -5,5 +5,9 @@ public class test{
   public static void main() {
     System.out.println("abcd");
     System.out.println("가나다라 마바사");
+    for(int i= 0; i< 10; i++){
+      System.out.println(i);
+    }
+    
   }
 }
